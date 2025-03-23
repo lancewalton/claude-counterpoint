9bd5d9125da31647d70f6a4314b6ebbe4fde9fb6
Lance Walton <461999+lancewalton@users.noreply.github.com>
2025-03-23 22:09:21 +0000

    Prompt: Add a new counterpoint rule to prohibit parallel octaves and their compounds

--- src/main/scala/counterpoint/CounterpointRules.scala
+++ src/main/scala/counterpoint/CounterpointRules.scala
@@ -52,3 +52,35 @@ case class CounterpointRules():
       // If voices move in different directions or one of them doesn't move, 
       // it's not parallel motion, so this rule doesn't apply
       true
+      
+  def noParallelOctavesAndCompoundsRule(
+    lowerVoiceLastNote: Note,
+    lowerVoiceCandidateNote: Note,
+    upperVoiceLastNote: Note,
+    upperVoiceCandidateNote: Note
+  ): Boolean =
+    val motionType = MotionType.getMotionType(
+      lowerVoiceLastNote,
+      lowerVoiceCandidateNote,
+      upperVoiceLastNote,
+      upperVoiceCandidateNote
+    )
+    
+    // If both voices move in parallel motion
+    if motionType == Type.Parallel then
+      // Get intervals and their simple form
+      val lastInterval = Interval.between(lowerVoiceLastNote, upperVoiceLastNote)
+      val candidateInterval = Interval.between(lowerVoiceCandidateNote, upperVoiceCandidateNote)
+      
+      val (lastSimpleName, _) = lastInterval.getSimpleForm
+      val (candidateSimpleName, _) = candidateInterval.getSimpleForm
+      
+      // Check for octaves in their simple form (handles compounds)
+      !(lastSimpleName == IntervalName.Octave && 
+        candidateSimpleName == IntervalName.Octave && 
+        lastInterval.quality == IntervalQuality.Perfect && 
+        candidateInterval.quality == IntervalQuality.Perfect)
+    else
+      // If voices move in different directions or one of them doesn't move, 
+      // it's not parallel motion, so this rule doesn't apply
+      true
--- src/test/scala/counterpoint/CounterpointRulesSpec.scala
+++ src/test/scala/counterpoint/CounterpointRulesSpec.scala
@@ -36,4 +36,75 @@ class CounterpointRulesSpec extends AnyFunSuite with Matchers {
     )
     result4 shouldBe true
   }
+  
+  test("noParallelPerfectOctavesRule should reject parallel perfect octaves") {
+    val rules = CounterpointRules()
+    
+    // Test with parallel perfect octaves (ascending)
+    // C4 to D4 in lower voice, C5 to D5 in upper voice (both are perfect octaves)
+    val result1 = rules.noParallelPerfectOctavesRule(
+      Note.C4, Note.D4, Note.C5, Note.D5
+    )
+    result1 shouldBe false
+    
+    // Test with parallel perfect octaves (descending)
+    // D4 to C4 in lower voice, D5 to C5 in upper voice (both are perfect octaves)
+    val result2 = rules.noParallelPerfectOctavesRule(
+      Note.D4, Note.C4, Note.D5, Note.C5
+    )
+    result2 shouldBe false
+    
+    // Test with non-parallel perfect octaves (contrary motion)
+    // C4 to D4 in lower voice, C5 to B4 in upper voice
+    val result3 = rules.noParallelPerfectOctavesRule(
+      Note.C4, Note.D4, Note.C5, Note.B4
+    )
+    result3 shouldBe true
+    
+    // Test with parallel motion but not perfect octaves
+    // C4 to D4 in lower voice, E4 to F4 in upper voice
+    val result4 = rules.noParallelPerfectOctavesRule(
+      Note.C4, Note.D4, Note.E4, Note.F4
+    )
+    result4 shouldBe true
+  }
+  
+  test("noParallelOctavesAndCompoundsRule should reject parallel octaves and their compounds") {
+    val rules = CounterpointRules()
+    
+    // Test with parallel perfect octaves
+    // C4 to D4 in lower voice, C5 to D5 in upper voice (perfect octaves)
+    val result1 = rules.noParallelOctavesAndCompoundsRule(
+      Note.C4, Note.D4, Note.C5, Note.D5
+    )
+    result1 shouldBe false
+    
+    // Test with parallel compound octaves (double octaves)
+    // C3 to D3 in lower voice, C5 to D5 in upper voice (double octaves)
+    val result2 = rules.noParallelOctavesAndCompoundsRule(
+      Note.C3, Note.D3, Note.C5, Note.D5
+    )
+    result2 shouldBe false
+    
+    // Test with parallel compound octaves (15th - double octave + perfect octave)
+    // C3 to D3 in lower voice, C5 to D5 in upper voice (15th interval)
+    val result3 = rules.noParallelOctavesAndCompoundsRule(
+      Note.C3, Note.D3, Note.C5, Note.D5
+    )
+    result3 shouldBe false
+    
+    // Test with non-parallel compound octaves (contrary motion)
+    // C3 to D3 in lower voice, C5 to B4 in upper voice
+    val result4 = rules.noParallelOctavesAndCompoundsRule(
+      Note.C3, Note.D3, Note.C5, Note.B4
+    )
+    result4 shouldBe true
+    
+    // Test with parallel motion but not octaves/compounds
+    // C4 to D4 in lower voice, E4 to F4 in upper voice (major 3rds)
+    val result5 = rules.noParallelOctavesAndCompoundsRule(
+      Note.C4, Note.D4, Note.E4, Note.F4
+    )
+    result5 shouldBe true
+  }
 }
